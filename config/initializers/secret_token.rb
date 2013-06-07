ff2b5c747538810268b6e5269ada599b31bc763d
Aslan GULTEKIN <whowantstolivefo@gmail.com>
2013-06-07 23:21:43 +0300

    static2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Static2::Application.config.secret_token = '4b96f53ef589a922e2d8cbc9ab917cdab40332edd9fd729b1abe236313c67c5ea495de3a91911705efc89a6c78824e2bcb3c598081c9745e1885ffc05a24715c'
